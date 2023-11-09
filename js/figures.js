175d707402a9fd5b2600953a1580f2691b643496
Ole KH <omhagberg@uia.no>
2023-11-09 17:38:47 +0100

    litt kode...

--- js/figures.js
+++ js/figures.js
@@ -1,7 +1,40 @@
-function circle(radius, mode, color) {
+function circle(radius, mode, color, canvasId = "canvas") {
+    const canvas = document.getElementById(canvasId);
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        ctx.fillstyle = color;
 
+        switch(mode) {
+            case "solid":
+                ctx.fillRect(0, 0, )
+            case "outline":
+
+        }
+    }
 }
 
-function rectangle(width, heigth, mode, color) {
+function circleSVG(radius, mode, color) {
+    
+}
+
+function rectangle(width, heigth, mode, color, canvasId = "canvas") {
+    const canvas = document.getElementById(canvasId);
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        ctx.fillstyle = color;
+
+        switch(mode) {
+            case "solid":
+                ctx.fillRect(0, 0, width, heigth)
+            case "outline":
+                ctx.strokeRect(0, 0, width, heigth)
+        }
+    }
+    else {
+        throw new Error("Canvas doesn't match given ID")
+    }
+}
+
+function rectangleSVG(width, heigth, mode, color) {
 
 }

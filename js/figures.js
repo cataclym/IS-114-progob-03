9f9fdfaf730ca1c6ee8fad80c6d2dbc729db5078
Ole KH <omhagberg@uia.no>
2023-11-16 16:20:04 +0100

    WIP stjerner

--- js/figures.js
+++ js/figures.js
@@ -82,6 +82,49 @@ function rectangleOutlineSVG(width, height, color, outlineWidth, positionX = 0, 
     return `<rect width="${width}" height="${height}" x="${positionX}" y="${positionY}" stroke="${color}" stroke-width="${outlineWidth}" fill="rgba(0, 0, 0, 0)" />`
 }
 
+function star(size, color, canvasID) {
+    const canvas = document.getElementById(canvasID);
+
+    const cx = 75 * size,
+    cy = 100 * size,
+    spikes = 5 * size,
+    outerRadius = 30 * size,
+    innerRadius = 15 * size;
+
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+
+        let rot= Math.PI/2*3;
+        let x = cx;
+        let y = cy;
+        let step = Math.PI / spikes;
+
+        ctx.beginPath();
+        ctx.moveTo(cx, cy - outerRadius)
+
+        for(let i= 0; i < spikes; i++){
+            x = cx + Math.cos(rot) * outerRadius;
+            y = cy + Math.sin(rot) * outerRadius;
+            ctx.lineTo(x, y)
+            rot += step
+
+            x= cx + Math.cos(rot) * innerRadius;
+            y= cy + Math.sin(rot) * innerRadius;
+            ctx.lineTo(x, y)
+            rot += step
+        }
+
+        ctx.lineTo(cx, cy - outerRadius);
+        ctx.closePath();
+        ctx.fillStyle = color;
+        ctx.fill();
+    }
+}
+
+function starSVG(size = 1, color) {
+    return `<polygon points="${100 * size}, ${10 * size} ${40 * size},${198* size} ${190 * size},${78 * size} ${10 * size},${78 * size} ${160 * size},${198 * size}" fill="${color}" />`
+}
+
 export {
     circle,
     circleSVG,
@@ -90,5 +133,7 @@ export {
     rectangle,
     rectangleSVG,
     rectangleOutline,
-    rectangleOutlineSVG
+    rectangleOutlineSVG,
+    star,
+    starSVG
 }

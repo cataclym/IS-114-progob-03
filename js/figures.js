309d9c7c441940181d2ee765c1535b1c5b6c3571
Ole KH <omhagberg@uia.no>
2023-11-16 14:07:47 +0100

    oppdatert lib

--- js/figures.js
+++ js/figures.js
@@ -1,4 +1,43 @@
-function circle(radius, mode, color, canvasId = "canvas", positionX = 0, positionY = 0, outlineWidth = 5) {
+function circle(radius, color, canvasId = "canvas", positionX = 0, positionY = 0) {
+    const canvas = document.getElementById(canvasId);
+
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        ctx.beginPath()
+        ctx.fillStyle = color;
+        ctx.arc(positionX, positionY, radius, 0, Math.PI * 2, false);
+        ctx.fill();
+        return ctx.closePath()
+    }
+    else {
+        throw new Error("Canvas doesn't match given ID")
+    }
+}
+
+function circleSVG(radius, color, positionX = 0, positionY = 0) {
+    return `<circle cx="${positionX}" cy="${positionY}" r="${radius}" fill="${color}" />`
+}
+
+function rectangle(width, height, color, canvasId = "canvas", positionX = 0, positionY = 0) {
+    const canvas = document.getElementById(canvasId);
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        ctx.beginPath()
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
+        ctx.fillRect(positionX, positionY, width, height);
+        return ctx.closePath();
+    }
+    else {
+        throw new Error("Canvas doesn't match given ID")
+    }
+}
+
+function rectangleSVG(width, height, color, positionX = 0, positionY = 0) {
+    return `<rect width="${width}" height="${height}" x="${positionX}" y="${positionY}" fill="${color}" />`
+}
+
+function circleOutline(radius, color, outlineWidth, canvasId, positionX, positionY) {
     const canvas = document.getElementById(canvasId);
 
     if (canvas.getContext) {
@@ -8,64 +47,48 @@ function circle(radius, mode, color, canvasId = "canvas", positionX = 0, positio
         ctx.strokeStyle = color;
         ctx.lineWidth = outlineWidth;
         ctx.arc(positionX, positionY, radius, 0, Math.PI * 2, false);
-
-        switch(mode) {
-            default:
-            case "solid":
-                ctx.fill();
-                break;
-            case "outline":
-                ctx.stroke();
-                break;
-        }
-        return ctx.closePath()
+        ctx.stroke();
+        return ctx.closePath();
     }
     else {
         throw new Error("Canvas doesn't match given ID")
     }
 }
 
-function circleSVG(radius, mode, color, positionX = 0, positionY = 0, outlineWidth = 5) {
-    switch (mode) {
-        case "outline":
-            return `<circle cx="${positionX}" cy="${positionY}" r="${radius}" stroke="${color}" fill="rgba(0, 0, 0, 0)" stroke-width="${outlineWidth}" />`
-        default:
-        case "solid":
-            return `<circle cx="${positionX}" cy="${positionY}" r="${radius}" fill="${color}" />`
-    }
+function circleOutlineSVG(radius, color, outlineWidth, positionX = 0, positionY = 0) {
+    return `<circle cx="${positionX}" cy="${positionY}" r="${radius}" stroke="${color}" fill="rgba(0, 0, 0, 0)" stroke-width="${outlineWidth}" />`
 }
 
-function rectangle(width, height, mode, color, canvasId = "canvas", positionX = 0, positionY = 0) {
+function rectangleOutline(width, height, color, outlineWidth, canvasId, positionX = 0, positionY = 0) {
     const canvas = document.getElementById(canvasId);
+
     if (canvas.getContext) {
         const ctx = canvas.getContext("2d");
+        ctx.beginPath()
         ctx.fillStyle = color;
         ctx.strokeStyle = color;
+        ctx.lineWidth = outlineWidth;
+        ctx.strokeRect(positionX, positionY, width, height)
+        ctx.stroke();
 
-        switch(mode) {
-            case "solid":
-                ctx.fillRect(positionX, positionY, width, height)
-                return;
-            case "outline":
-                ctx.strokeRect(positionX, positionY, width, height)
-                return;
-            default:
-                ctx.clearRect(positionX, positionY, width, height);
-        }
+        return ctx.closePath();
     }
     else {
         throw new Error("Canvas doesn't match given ID")
     }
 }
 
-function rectangleSVG(width, heigth, mode, color, positionX = 0, positionY = 0) {
-    switch (mode) {
-        case "outline":
-            return `<rect width="${width}" height="${heigth}" x="${positionX}" y="${positionY}" stroke="${color}" fill="rgba(0, 0, 0, 0)" />`
-        default:
-        case "solid":
-            return `<rect width="${width}" height="${heigth}" x="${positionX}" y="${positionY}" fill="${color}" />`
-    }
+function rectangleOutlineSVG(width, height, color, outlineWidth, positionX = 0, positionY = 0) {
+    return `<rect width="${width}" height="${height}" x="${positionX}" y="${positionY}" stroke="${color}" stroke-width="${outlineWidth}" fill="rgba(0, 0, 0, 0)" />`
 }
 
-export { circle, circleSVG, rectangle, rectangleSVG }
+export {
+    circle,
+    circleSVG,
+    circleOutline,
+    circleOutlineSVG,
+    rectangle,
+    rectangleSVG,
+    rectangleOutline,
+    rectangleOutlineSVG
+}

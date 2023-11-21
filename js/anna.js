72216c3f8d77c494632a78eaa8fcf9ac0985c974
anna oestmoen <annamoe03@hotmail.com>
2023-11-21 12:39:20 +0100

    laget svg figur

--- js/anna.js
+++ js/anna.js
@@ -1,4 +1,4 @@
-import {circle, star} from "./figures.js"
+import {circle, circleSVG, star, starSVG} from "./figures.js"
 
 export function drawStar(canvasID) {
     const canvas = document.getElementById(canvasID);
@@ -15,3 +15,18 @@ export function drawStar(canvasID) {
 }
 
 drawStar("anna")
+
+export function drawStarSVG(svgID) {
+    const svgElement = document.getElementById(svgID);
+
+    const sirkel1 = circleSVG(150, "#000080", canvasID, 150, 150),
+    sirkel2 = circleSVG(100, "#00008B", canvasID, 150, 150),
+    sirkel3 = starSVG(4.5, "#ffd700", canvasID, 150, 150),
+    sirkel4 = starSVG(2, "#Ffff00", canvasID, 150, 150)
+
+    svgElement.innerHTML += `${sirkel1} ${sirkel2} ${sirkel3} ${sirkel4}`;
+
+    
+}
+
+drawStarSVG("annaSVG")

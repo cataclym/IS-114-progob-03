01a93668e125be27e14c46ed52c22a28fd1fdd04
Ole KH <omhagberg@uia.no>
2023-11-21 13:41:36 +0100

    Fikset Marthe sin figur

--- js/marthe.js
+++ js/marthe.js
@@ -1,5 +1,5 @@
 
-import {rectangle} from "./figures.js"
+import {circle, rectangle} from "./figures.js"
 
 export function drawNisse(canvasID) {
     const canvas = document.getElementById(canvasID);
@@ -9,17 +9,17 @@ export function drawNisse(canvasID) {
     ctx.canvas.height = 300
 
 //base
-    rectangle(300, 300, "#B22222", canvasID, 300, 300)
+    rectangle(300, 300, "#B22222", canvasID, 0, 0)
 
 //belte
-    rectangle(300, 32, "#000000", canvasID, 150, 200)
-    rectangle(300, 30, "#F5F5F5", canvasID, 150, 200)
+    rectangle(300, 34, "#000000", canvasID, 0, 198)
+    rectangle(300, 30, "#F5F5F5", canvasID, 0, 200)
 
 //beltespenne
-rectangle(57, 57, "#000000", canvasID, 150, 200)
-rectangle(55, 55, "#DAA520", canvasID, 150, 200)
-rectangle(37, 37, "#000000", canvasID, 150, 200)
-rectangle(35, 35, "#F5F5F5", canvasID, 150, 200)
+rectangle(78, 78, "#000000", canvasID, 113.5, 178.5)
+    rectangle(75, 75, "#DAA520", canvasID, 115, 180)
+rectangle(38, 38, "#000000", canvasID, 133.5, 198.5)
+rectangle(35, 35, "#F5F5F5", canvasID, 135, 200)
 
 //knapper
     circle(16, "#000000", canvasID, 150, 30)
@@ -28,7 +28,6 @@ rectangle(35, 35, "#F5F5F5", canvasID, 150, 200)
     circle(15, "#DAA520", canvasID, 150, 75)
     circle(16, "#000000", canvasID, 150, 120)
     circle(15, "#DAA520", canvasID, 150, 120)
- 
 }
  
 drawNisse("marthe")
